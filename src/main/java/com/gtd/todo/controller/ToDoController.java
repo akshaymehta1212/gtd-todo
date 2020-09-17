64630ffea4d157e47312bad43f06947d7f3c9f8f
akshaymehta1212 <akshaymehta120@gmail.com>
2020-09-17 21:51:37 +0530

    commented some test and removed unused dependency

--- src/main/java/com/gtd/todo/controller/ToDoController.java
+++ src/main/java/com/gtd/todo/controller/ToDoController.java
@@ -2,11 +2,8 @@ package com.gtd.todo.controller;
 
 import com.gtd.todo.model.ToDoItem;
 import com.gtd.todo.model.User;
-import com.gtd.todo.repository.ToDoItemRepository;
-import com.gtd.todo.repository.UserRepository;
 import com.gtd.todo.service.ToDoService;
 import com.gtd.todo.service.UserService;
-import jdk.management.resource.ResourceRequestDeniedException;
 import lombok.Data;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,8 +25,6 @@ public class ToDoController {
 
     private final ToDoService toDoService;
     private final UserService userService;
-    private final UserRepository userRepository;
-    private final ToDoItemRepository toDoItemRepository;
 
     @GetMapping("/users/{userId}/toDoItems")
     public List<ToDoItem> getAllTasksById(@PathVariable(value = "userId") User userId) {
@@ -48,12 +43,10 @@ public class ToDoController {
         return toDoService.saveOrUpdate(toDoItem);
     }
 
-    @PutMapping("users/{userId}/toDoItems/")
+    @PutMapping("users/{userId}/toDoItems")
     public ToDoItem updateToDo(@PathVariable(value = "userId") int userId,
                                @RequestBody ToDoItem toDoItemRequest) {
-        if (!userRepository.existsById(userId)) {
-            throw new ResourceRequestDeniedException("user id" + userId + "not valid");
-        }
+        toDoItemRequest.setUser(userService.getUserById(userId));
         return toDoService.saveOrUpdate(toDoItemRequest);
     }
 
