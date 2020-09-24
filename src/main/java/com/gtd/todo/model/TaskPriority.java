475ab44be071177eff2f08da3d9f2e772d35f25f
Parth Mehta <parthmehta009@gmail.com>
2020-09-24 20:01:02 +0530

    Added JsonCreator and JsonValue annotation for Enum

--- src/main/java/com/gtd/todo/model/TaskPriority.java
+++ src/main/java/com/gtd/todo/model/TaskPriority.java
@@ -1,5 +1,10 @@
 package com.gtd.todo.model;
 
+import java.util.stream.Stream;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum TaskPriority {
     LOW("Low"),
     MEDIUM("Medium"),
@@ -11,8 +16,17 @@ public enum TaskPriority {
         this.priority = priority;
     }
 
+    @JsonValue
     public String getPriority() {
         return priority;
+    }
+    
+    @JsonCreator
+    public static TaskPriority decode(final String taskPriority) {
+    	return Stream.of(TaskPriority.values())
+                .filter(priority -> priority.getPriority().equals(taskPriority))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
     }
 
     @Override
--- src/main/java/com/gtd/todo/model/ToDoItem.java
+++ src/main/java/com/gtd/todo/model/ToDoItem.java
@@ -1,14 +1,21 @@
 package com.gtd.todo.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import javafx.concurrent.Task;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
 import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "todoitems")
@@ -20,15 +27,12 @@ public class ToDoItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int taskId;
-    @Convert(converter = TaskPriorityConverter.class)
     private TaskPriority priority;
     private String taskName;
     private String taskDetails;
     private Date createdOn;
     private Boolean isDone;
     private Date lastUpdated;
-
-
 
     @ManyToOne
     @JoinColumn(name = "user_id")
